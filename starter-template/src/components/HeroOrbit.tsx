5c7026f0aa4723b0e1b77e63463bc9a75b27b1a5
Akshat Jain <jnakshat202@gmail.com>
2025-03-27 22:08:39 +0530

    .

--- starter-template/src/components/HeroOrbit.tsx
+++ starter-template/src/components/HeroOrbit.tsx
@@ -2,13 +2,13 @@ import { PropsWithChildren } from "react"
 
 export const HeroOrbit = ({children, size, rotation} : PropsWithChildren<{size : number; rotation: number; }>) => {
     return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
-      <div className="border border-red-500 " style = {{
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2  ">
+      <div className="flex justify-start items-start" style = {{
         transform: `rotate(${rotation}deg)`,
         width: `${size}px`,
         height: `${size}px`
       }}>
-        <div className="border border-red-500 inline-flex " style = {{
+        <div className="inline-flex " style = {{
             transform: `rotate(${rotation * -1 }deg)`
         }}>
           {children}
--- starter-template/src/sections/Hero.tsx
+++ starter-template/src/sections/Hero.tsx
@@ -4,26 +4,53 @@ import ArrowDown from "@/assets/icons/arrow-down.svg";
 import grainImage from "@/assets/images/grain.jpg"; 
 import StarIcon from "@/assets/icons/star.svg";
 import { HeroOrbit } from "@/components/HeroOrbit";
+import SparkleIcon from "@/assets/icons/sparkle.svg";
+import { useEffect } from "react";
+
 
 export const HeroSection = () => {
   return (
   <div className="py-32 md:py-42 lg:py-62 relative z-0 overflow-x-clip">
-    <div className="absolute inset-0 -z-30 opacity-5" style = {{
-      backgroundImage: `url(${grainImage.src})`
+    <div className="absolute inset-0 [mask-image:linear-gradient(to_top,_transparent,_black_50%,_black_100%,_transparent)]">
+      <div className="absolute inset-0 -z-30 opacity-5" style = {{
+      backgroundImage: `url(${grainImage.src})` 
       
-    }}></div>
-    <div className="size-[620px] hero-ring"></div>
-    <div className="size-[820px] hero-ring"></div>
-    <div className="size-[1020px] hero-ring"></div>
-    <div className="size-[1220px] hero-ring"></div>
-    <HeroOrbit size = {800} rotation = {-72}>
-          <StarIcon className = " size-28 text-emerald-300" />
-    </HeroOrbit>
-
-    <HeroOrbit size = {550} rotation = {20}>
-          <StarIcon className = " size-12  text-emerald-300" />
-    </HeroOrbit>
-
+      }}></div>
+      <div className="size-[620px] hero-ring"></div>
+      <div className="size-[820px] hero-ring"></div>
+      <div className="size-[1020px] hero-ring"></div>
+      <div className="size-[1220px] hero-ring"></div>
+      <HeroOrbit size = {800} rotation = {-72}>
+        <StarIcon className = " size-28 text-emerald-300" />
+      </HeroOrbit>
+      <HeroOrbit size = {550} rotation = {20}>
+        <StarIcon className = " size-12  text-emerald-300" />
+      </HeroOrbit>
+      <HeroOrbit size = {590} rotation = {98}>
+        <StarIcon className = " size-12  text-emerald-300" />
+      </HeroOrbit>
+      <HeroOrbit size = {430} rotation = {-14}>
+        <SparkleIcon className = " size-8  text-emerald-300/4" />
+      </HeroOrbit>
+      <HeroOrbit size = {440} rotation = {80}>
+        <SparkleIcon className = " size-5  text-emerald-800" />
+      </HeroOrbit>  
+      <HeroOrbit size = {530} rotation = {180}>
+        <SparkleIcon className = " size-10  text-emerald-800" />
+      </HeroOrbit>
+      <HeroOrbit size = {710} rotation = {145}>
+        <SparkleIcon className = " size-14  text-emerald-800" />
+      </HeroOrbit>
+      <HeroOrbit size = {720} rotation = {90}>
+        <div className = " size-3 rounded-full  bg-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size = {520} rotation = {-45}>
+        <div className = " size-2 rounded-full  bg-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size = {650} rotation = {-5}>
+        <div className = " size-2 rounded-full  bg-emerald-300/20" />
+      </HeroOrbit>
+    </div>
     <div className="container">
       <div className="flex flex-col items-center gap-4">
         <Image src = {memojiImage} className = " size-[100px]" alt="memoji" width={200} height={200} /> 
